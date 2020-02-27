11cd51ef6eedb3d071f913e5e4eb98d4ad042c8b
Mohammed  Chowdhury <chowdhury1478@gmail.com>
2020-02-27 05:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ function sayHiToGrandma(string){
   if(string.toLowerCase == string){
     return "I can\'t hear you!"
   }
-  if(string == "I love you, Grandma"){
-    return "I love you, too."
-  }
+  
   if(string.toUpperCase() == string){
     return "YES INDEED!"
   }
+  
+  if(string == "I love you, Grandma"){
+    return "I love you, too."
+  }
+  
   
   
 }
